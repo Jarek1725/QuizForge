709a7bce25f8b5cd69951acc44692d31333073dc
jarek1725 <tomaszewskijarek1725@gmail.com>
2025-03-18 20:34:11 +0100

    first commit

---
+++ src/main/java/tomaszewski/QuizForge/QuizForgeApplication.java
@@ -0,0 +1,13 @@
+package tomaszewski.QuizForge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizForgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tomaszewski/QuizForge/QuizForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package tomaszewski.QuizForge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
