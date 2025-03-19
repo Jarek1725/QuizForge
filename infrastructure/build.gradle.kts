01b08f392166b29b415a8e547ad7fa2461b2e4d0
jarek1725 <tomaszewskijarek1725@gmail.com>
2025-03-19 19:37:08 +0100

    add lombok

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -12,8 +12,10 @@ repositories {
 dependencies {
     implementation(project(":domain"))
     implementation("org.postgresql:postgresql:42.6.0")
+    implementation("org.projectlombok:lombok:1.18.32")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.4")
     implementation("org.springframework.boot:spring-boot-starter-web:3.2.4")
+    implementation("org.springframework.boot:spring-boot-starter-security:3.2.4")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
