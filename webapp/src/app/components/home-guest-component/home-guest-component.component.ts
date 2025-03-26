d0f7461c78de740d40c87b22ed2a4e0d460853da
jarek1725 <tomaszewskijarek1725@gmail.com>
2025-03-26 20:55:54 +0100

    create-login-panel

--- webapp/src/app/components/home-guest-component/home-guest-component.component.ts
+++ webapp/src/app/components/home-guest-component/home-guest-component.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import {MatButton} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import {LoginPanelComponent} from '../login-panel/login-panel.component';
 
 @Component({
   selector: 'app-home-guest-component',
-  imports: [],
+  imports: [
+    MatButton,
+    MatIconModule,
+    LoginPanelComponent
+  ],
   templateUrl: './home-guest-component.component.html',
   styleUrl: './home-guest-component.component.scss'
 })
