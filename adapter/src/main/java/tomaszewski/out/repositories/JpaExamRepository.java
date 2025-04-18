9b350239907aa470db34d1ef90a7e5f4c59d3460
jarek1725 <tomaszewskijarek1725@gmail.com>
2025-04-18 17:50:15 +0200

    update jpa

--- adapter/src/main/java/tomaszewski/out/repositories/JpaExamRepository.java
+++ adapter/src/main/java/tomaszewski/out/repositories/JpaExamRepository.java
@@ -13,14 +13,17 @@ import java.util.List;
 
 public interface JpaExamRepository extends JpaRepository<ExamEntity, Long> {
     @Query(value = """
-    SELECT e.* FROM exam AS e
-    INNER JOIN exam_categories AS ec ON e.id = ec.exam_id
-    INNER JOIN categories AS c ON ec.category_id = c.id
-    INNER JOIN university AS u ON e.university_id = u.id
-    WHERE (:category IS NULL OR c.name = :category)
-      AND (:university IS NULL OR u.name = :university)
-    LIMIT :limit
-""", nativeQuery = true)
+                SELECT e.*
+                FROM exam e
+                INNER JOIN exam_categories ec ON e.id = ec.exam_id
+                INNER JOIN categories c ON ec.category_id = c.id
+                INNER JOIN university u ON e.university_id = u.id
+                WHERE (:category IS NULL OR c.name = :category)
+                  AND (:university IS NULL OR u.name = :university)
+                GROUP BY e.id
+                ORDER BY e.id DESC
+                LIMIT :limit
+            """, nativeQuery = true)
     List<ExamEntity> findExamsWithFilters(
             @Param("category") String category,
             @Param("university") String university,
